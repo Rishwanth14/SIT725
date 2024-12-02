e95ac50cf9396a6b46243ae8ba104a717db073c5
Rishwanth14 <rishwanthmathi@gmail.com>
2024-12-02 22:37:26 +1100

    b

---
+++ WEEK 3/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001; // You can choose a different port if needed
+
+// Serve static files (HTML, CSS, JS) from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Default route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
